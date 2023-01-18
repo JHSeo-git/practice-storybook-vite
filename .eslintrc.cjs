2c0fc7f1d6cac4f8ab5169edb15a176a72f58c7c
JHSeo <qnfqnfqnf@gmail.com>
2023-01-18 22:16:21 +0900

    initialize
    - storybook + vite + react
    - eslint
    - prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: [
+    'plugin:prettier/recommended',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['json-format', 'prettier', 'react', 'simple-import-sort', '@typescript-eslint'],
+  rules: {
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': 'error',
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      {
+        prefer: 'type-imports',
+        disallowTypeAnnotations: false,
+      },
+    ],
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-empty-interface': 'warn',
+  },
+};
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # practice-storybook-vite
+
 Practice for React + Vite + Storybook
+
+## vite + storybook init
+
+### 1. create vite app
+
+```bash
+yarn create vite
+
+# [project-name]
+# - React
+# - TypeScript + SWC
+```
+
+### 2. install dependencies
+
+```bash
+cd [project-name]
+yarn install
+```
+
+### 3. init storybook
+
+```bash
+npx sb init --builder @storybook/builder-vite
+```
