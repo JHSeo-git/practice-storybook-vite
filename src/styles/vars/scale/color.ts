f088c3e75d0bfca4c43d10aed96f1bc500a9c6b1
JHSeo <qnfqnfqnf@gmail.com>
2023-01-20 22:05:40 +0900

    feat: add color for design-token

---
+++ src/styles/vars/scale/color.ts
@@ -0,0 +1,164 @@
+// https://www.radix-ui.com/docs/colors/palette-composition/the-scales
+
+export const black = '#000';
+export const white = '#fff';
+
+// light
+
+export const gray1 = '#fcfcfc';
+export const gray2 = '#f8f8f8';
+export const gray3 = '#f3f3f3';
+export const gray4 = '#ededed';
+export const gray5 = '#e8e8e8';
+export const gray6 = '#e2e2e2';
+export const gray7 = '#dbdbdb';
+export const gray8 = '#c7c7c7';
+export const gray9 = '#8f8f8f';
+export const gray10 = '#858585';
+export const gray11 = '#6f6f6f';
+export const gray12 = '#171717';
+
+export const red1 = '#fffcfc';
+export const red2 = '#fff8f8';
+export const red3 = '#ffefef';
+export const red4 = '#ffe5e5';
+export const red5 = '#fdd8d8';
+export const red6 = '#f9c6c6';
+export const red7 = '#f3aeaf';
+export const red8 = '#eb9091';
+export const red9 = '#e5484d';
+export const red10 = '#dc3d43';
+export const red11 = '#cd2b31';
+export const red12 = '#381316';
+
+export const green1 = '#fbfefc';
+export const green2 = '#f2fcf5';
+export const green3 = '#e9f9ee';
+export const green4 = '#ddf3e4';
+export const green5 = '#ccebd7';
+export const green6 = '#b4dfc4';
+export const green7 = '#92ceac';
+export const green8 = '#5bb98c';
+export const green9 = '#30a46c';
+export const green10 = '#299764';
+export const green11 = '#18794e';
+export const green12 = '#153226';
+
+export const blue1 = '#fbfdff';
+export const blue2 = '#f5faff';
+export const blue3 = '#edf6ff';
+export const blue4 = '#e1f0ff';
+export const blue5 = '#cee7fe';
+export const blue6 = '#b7d9f8';
+export const blue7 = '#96c7f2';
+export const blue8 = '#5eb0ef';
+export const blue9 = '#0091ff';
+export const blue10 = '#0081f1';
+export const blue11 = '#006adc';
+export const blue12 = '#00254d';
+
+export const yellow1 = '#fdfdf9';
+export const yellow2 = '#fffce8';
+export const yellow3 = '#fffbd1';
+export const yellow4 = '#fff8bb';
+export const yellow5 = '#fef2a4';
+export const yellow6 = '#f9e68c';
+export const yellow7 = '#efd36c';
+export const yellow8 = '#ebbc00';
+export const yellow9 = '#f5d90a';
+export const yellow10 = '#f7ce00';
+export const yellow11 = '#946800';
+export const yellow12 = '#35290f';
+
+export const violet1 = '#fdfcfe';
+export const violet2 = '#fbfaff';
+export const violet3 = '#f5f2ff';
+export const violet4 = '#ede9fe';
+export const violet5 = '#e4defc';
+export const violet6 = '#d7cff9';
+export const violet7 = '#c4b8f3';
+export const violet8 = '#aa99ec';
+export const violet9 = '#6e56cf';
+export const violet10 = '#644fc1';
+export const violet11 = '#5746af';
+export const violet12 = '#20134b';
+
+// dark
+
+export const grayDark1 = '#161616';
+export const grayDark2 = '#1c1c1c';
+export const grayDark3 = '#232323';
+export const grayDark4 = '#282828';
+export const grayDark5 = '#2e2e2e';
+export const grayDark6 = '#343434';
+export const grayDark7 = '#3e3e3e';
+export const grayDark8 = '#505050';
+export const grayDark9 = '#707070';
+export const grayDark10 = '#7e7e7e';
+export const grayDark11 = '#a0a0a0';
+export const grayDark12 = '#ededed';
+
+export const redDark1 = '#1f1315';
+export const redDark2 = '#291415';
+export const redDark3 = '#3c181a';
+export const redDark4 = '#481a1d';
+export const redDark5 = '#541b1f';
+export const redDark6 = '#671e22';
+export const redDark7 = '#822025';
+export const redDark8 = '#aa2429';
+export const redDark9 = '#e5484d';
+export const redDark10 = '#f2555a';
+export const redDark11 = '#ff6369';
+export const redDark12 = '#feecee';
+
+export const greenDark1 = '#0d1912';
+export const greenDark2 = '#0c1f17';
+export const greenDark3 = '#0f291e';
+export const greenDark4 = '#113123';
+export const greenDark5 = '#133929';
+export const greenDark6 = '#164430';
+export const greenDark7 = '#1b543a';
+export const greenDark8 = '#236e4a';
+export const greenDark9 = '#30a46c';
+export const greenDark10 = '#3cb179';
+export const greenDark11 = '#4cc38a';
+export const greenDark12 = '#e5fbeb';
+
+export const blueDark1 = '#0f1720';
+export const blueDark2 = '#0f1b2d';
+export const blueDark3 = '#10243e';
+export const blueDark4 = '#102a4c';
+export const blueDark5 = '#0f3058';
+export const blueDark6 = '#0d3868';
+export const blueDark7 = '#0a4481';
+export const blueDark8 = '#0954a5';
+export const blueDark9 = '#0091ff';
+export const blueDark10 = '#369eff';
+export const blueDark11 = '#52a9ff';
+export const blueDark12 = '#eaf6ff';
+
+export const yellowDark1 = '#1c1500';
+export const yellowDark2 = '#221a00';
+export const yellowDark3 = '#2c2100';
+export const yellowDark4 = '#352800';
+export const yellowDark5 = '#3e3000';
+export const yellowDark6 = '#493c00';
+export const yellowDark7 = '#594a05';
+export const yellowDark8 = '#705e00';
+export const yellowDark9 = '#f5d90a';
+export const yellowDark10 = '#ffef5c';
+export const yellowDark11 = '#f0c000';
+export const yellowDark12 = '#fffad1';
+
+export const violetDark1 = '#17151f';
+export const violetDark2 = '#1c172b';
+export const violetDark3 = '#251e40';
+export const violetDark4 = '#2c2250';
+export const violetDark5 = '#32275f';
+export const violetDark6 = '#392c72';
+export const violetDark7 = '#443592';
+export const violetDark8 = '#5842c3';
+export const violetDark9 = '#6e56cf';
+export const violetDark10 = '#7c66dc';
+export const violetDark11 = '#9e8cfc';
+export const violetDark12 = '#f1eefe';
---
+++ src/styles/vars/semantic/color.ts
@@ -0,0 +1,13 @@
+import { $scale } from '..';
+
+// light
+
+export const loContrast = $scale.color.white;
+export const hiContrast = $scale.color.gray12;
+export const primary = $scale.color.violet6;
+
+// dark
+
+export const loContrastDark = $scale.color.grayDark1;
+export const hiContrastDark = $scale.color.grayDark12;
+export const primaryDark = $scale.color.violetDark6;
