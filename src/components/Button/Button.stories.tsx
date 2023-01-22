990953e6aa2fa98a6830fa5f175e4a8896ba1fc7
JHSeo <qnfqnfqnf@gmail.com>
2023-01-22 22:38:23 +0900

    feat: add button story

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -15,30 +15,260 @@ const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 export const Basic = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Basic.args = {
-  intent: 'primary',
+  intent: 'neutral',
+  variant: 'solid',
   size: 'medium',
-  label: 'Button',
+  children: 'Label',
 };
 
 Basic.argTypes = {
   onClick: { action: 'clicked' },
 };
 
+export function All() {
+  return (
+    <>
+      <div className="flex flex-wrap gap-4 p-4">
+        <Button intent="primary" variant="solid">
+          Primary
+        </Button>
+        <Button intent="neutral" variant="solid">
+          Neutral
+        </Button>
+        <Button intent="primary" variant="weak">
+          Weak
+        </Button>
+        <Button intent="neutral" variant="weak">
+          Weak
+        </Button>
+        <Button intent="primary" variant="outline">
+          Outline
+        </Button>
+        <Button intent="neutral" variant="outline">
+          Outline
+        </Button>
+        <Button intent="primary" variant="ghost">
+          Outline
+        </Button>
+        <Button intent="neutral" variant="ghost">
+          Outline
+        </Button>
+      </div>
+      <div className="flex flex-wrap gap-4 p-4">
+        <Button intent="primary" variant="solid" rounded>
+          Primary
+        </Button>
+        <Button intent="neutral" variant="solid" rounded>
+          Neutral
+        </Button>
+        <Button intent="primary" variant="weak" rounded>
+          Weak
+        </Button>
+        <Button intent="neutral" variant="weak" rounded>
+          Weak
+        </Button>
+        <Button intent="primary" variant="outline" rounded>
+          Outline
+        </Button>
+        <Button intent="neutral" variant="outline" rounded>
+          Outline
+        </Button>
+        <Button intent="primary" variant="ghost" rounded>
+          Outline
+        </Button>
+        <Button intent="neutral" variant="ghost" rounded>
+          Outline
+        </Button>
+      </div>
+    </>
+  );
+}
+
 export function Intent() {
   return (
-    <div className="flex gap-2">
-      <Button intent="primary" label="Primary" />
-      <Button intent="secondary" label="Secondary" />
+    <div className="flex flex-wrap gap-2">
+      <Button intent="neutral">Neutral</Button>
+      <Button intent="primary">Primary</Button>
+      <Button intent="danger">Danger</Button>
+      <Button intent="success">Success</Button>
+      <Button intent="info">Info</Button>
+      <Button intent="warning">Warning</Button>
     </div>
+  );
+}
+
+export function Variant() {
+  return (
+    <>
+      <div className="mb-4 flex flex-wrap gap-2">
+        <Button intent="neutral" variant="solid">
+          Solid
+        </Button>
+        <Button intent="neutral" variant="weak">
+          Weak
+        </Button>
+        <Button intent="neutral" variant="ghost">
+          Ghost
+        </Button>
+        <Button intent="neutral" variant="outline">
+          Outline
+        </Button>
+      </div>
+      <div className="mb-4 flex flex-wrap gap-2">
+        <Button intent="primary" variant="solid">
+          Solid
+        </Button>
+        <Button intent="primary" variant="weak">
+          Weak
+        </Button>
+        <Button intent="primary" variant="ghost">
+          Ghost
+        </Button>
+        <Button intent="primary" variant="outline">
+          Outline
+        </Button>
+      </div>
+      <div className="mb-4 flex flex-wrap gap-2">
+        <Button intent="danger" variant="solid">
+          Solid
+        </Button>
+        <Button intent="danger" variant="weak">
+          Weak
+        </Button>
+        <Button intent="danger" variant="ghost">
+          Ghost
+        </Button>
+        <Button intent="danger" variant="outline">
+          Outline
+        </Button>
+      </div>
+      <div className="mb-4 flex flex-wrap gap-2">
+        <Button intent="success" variant="solid">
+          Solid
+        </Button>
+        <Button intent="success" variant="weak">
+          Weak
+        </Button>
+        <Button intent="success" variant="ghost">
+          Ghost
+        </Button>
+        <Button intent="success" variant="outline">
+          Outline
+        </Button>
+      </div>
+      <div className="mb-4 flex flex-wrap gap-2">
+        <Button intent="info" variant="solid">
+          Solid
+        </Button>
+        <Button intent="info" variant="weak">
+          Weak
+        </Button>
+        <Button intent="info" variant="ghost">
+          Ghost
+        </Button>
+        <Button intent="info" variant="outline">
+          Outline
+        </Button>
+      </div>
+      <div className="mb-4 flex flex-wrap gap-2">
+        <Button intent="warning" variant="solid">
+          Solid
+        </Button>
+        <Button intent="warning" variant="weak">
+          Weak
+        </Button>
+        <Button intent="warning" variant="ghost">
+          Ghost
+        </Button>
+        <Button intent="warning" variant="outline">
+          Outline
+        </Button>
+      </div>
+    </>
   );
 }
 
 export function Size() {
   return (
-    <div className="flex gap-2">
-      <Button intent="primary" size="small" label="Small" />
-      <Button intent="primary" size="medium" label="Medium" />
-      <Button intent="primary" size="large" label="Large" />
+    <div className="flex items-start gap-2">
+      <Button intent="primary" size="small">
+        Small
+      </Button>
+      <Button intent="primary" size="medium">
+        Medium
+      </Button>
+      <Button intent="primary" size="large">
+        Large
+      </Button>
+    </div>
+  );
+}
+
+export function Rounded() {
+  return (
+    <div className="flex items-start gap-2">
+      <Button rounded>Neutral</Button>
+      <Button rounded intent="primary">
+        Primary
+      </Button>
+      <Button rounded intent="danger">
+        Danger
+      </Button>
+      <Button rounded intent="success">
+        Success
+      </Button>
+      <Button rounded intent="info">
+        Info
+      </Button>
+      <Button rounded intent="warning">
+        Warning
+      </Button>
+    </div>
+  );
+}
+
+export function Loading() {
+  return (
+    <div className="flex flex-wrap items-start gap-2">
+      <Button loading>Loading...</Button>
+      <Button loading intent="primary">
+        Loading...
+      </Button>
+      <Button loading intent="danger">
+        Loading...
+      </Button>
+      <Button loading intent="success">
+        Loading...
+      </Button>
+      <Button loading intent="info">
+        Loading...
+      </Button>
+      <Button loading intent="warning">
+        Loading...
+      </Button>
+    </div>
+  );
+}
+
+export function Disabled() {
+  return (
+    <div className="flex items-start gap-2">
+      <Button disabled>Neutral</Button>
+      <Button disabled intent="primary">
+        Primary
+      </Button>
+      <Button disabled intent="danger">
+        Danger
+      </Button>
+      <Button disabled intent="success">
+        Success
+      </Button>
+      <Button disabled intent="info">
+        Info
+      </Button>
+      <Button disabled intent="warning">
+        Warning
+      </Button>
     </div>
   );
 }
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,18 +1,32 @@
 import { cva } from 'class-variance-authority';
 
+import { cn } from '../../lib/utils/styleUtils';
+
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   /**
    * Is this the principal call to action on the page?
    */
-  intent: 'primary' | 'secondary';
+  intent?: 'neutral' | 'primary' | 'success' | 'danger' | 'info' | 'warning';
+  /**
+   * What the variant to use
+   */
+  variant?: 'solid' | 'weak' | 'outline' | 'ghost';
   /**
    * How large should the button be?
    */
   size?: 'small' | 'medium' | 'large';
   /**
-   * Button contents
+   * Is rounded?
    */
-  label: string;
+  rounded?: boolean;
+  /**
+   * Is disabled?
+   */
+  disabled?: boolean;
+  /**
+   * Is loading?
+   */
+  loading?: boolean;
   /**
    * Optional click handler
    */
@@ -22,31 +36,171 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 /**
  * Primary UI component for user interaction
  */
-export const Button = ({ intent, size = 'medium', label, ...props }: ButtonProps) => {
+export const Button = ({
+  intent = 'neutral',
+  variant = 'solid',
+  size = 'medium',
+  rounded = false,
+  disabled = false,
+  loading = false,
+  children = 'Label',
+  className,
+  type = 'button',
+  ...props
+}: ButtonProps) => {
   return (
-    <button type="button" className={button({ intent, size })} {...props}>
-      {label}
+    <button
+      type={type}
+      className={cn(button({ intent, variant, size, rounded, disabled, loading }), className)}
+      disabled={disabled}
+      {...props}
+    >
+      {children}
     </button>
   );
 };
 
-const button = cva(
-  ['font-semibold', 'border', 'rounded', 'inline-flex items-center justify-center'],
-  {
-    variants: {
-      intent: {
-        primary: ['bg-blue-500', 'text-white', 'border-blue-500'],
-        secondary: ['bg-white', 'text-blue-500', 'border-blue-500'],
-      },
-      size: {
-        small: ['text-sm', 'h-8', 'py-1', 'px-2'],
-        medium: ['text-base', 'h-10', 'py-2', 'px-4'],
-        large: ['text-lg', 'h-12', 'py-3', 'px-6'],
-      },
-      defaultVariants: {
-        intent: 'primary',
-        size: 'medium',
-      },
+const button = cva(['font-semibold rounded inline-flex items-center justify-center transition'], {
+  variants: {
+    intent: {
+      neutral: ['bg-base-11 hover:bg-base-12'],
+      primary: ['bg-primary-base hover:bg-primary-10'],
+      danger: ['bg-danger-base hover:bg-danger-10'],
+      success: ['bg-success-base hover:bg-success-10'],
+      info: ['bg-info-base hover:bg-info-10'],
+      warning: ['bg-warning-base hover:bg-warning-10'],
     },
-  }
-);
+    variant: {
+      solid: ['text-base-lo'],
+      weak: [],
+      ghost: ['bg-transparent'],
+      outline: ['border'],
+    },
+    size: {
+      small: ['text-sm py-1 px-2'],
+      medium: ['text-base py-1.5 px-4'],
+      large: ['text-lg py-2 px-6'],
+    },
+    rounded: {
+      true: ['rounded-full'],
+    },
+    disabled: {
+      true: ['opacity-50 pointer-events-none'],
+    },
+    loading: {
+      true: ['opacity-70 pointer-events-none'],
+    },
+  },
+  compoundVariants: [
+    // solid
+    {
+      intent: 'warning',
+      variant: 'solid',
+      class: ['text-black'],
+    },
+    // weak
+    {
+      intent: 'neutral',
+      variant: 'weak',
+      class: ['bg-base-4 text-base-hi hover:bg-base-5 active:bg-base-6'],
+    },
+    {
+      intent: 'primary',
+      variant: 'weak',
+      class: ['bg-primary-4 text-primary-hi hover:bg-primary-5 active:bg-primary-6'],
+    },
+    {
+      intent: 'danger',
+      variant: 'weak',
+      class: ['bg-danger-4 text-danger-hi hover:bg-danger-5 active:bg-danger-6'],
+    },
+    {
+      intent: 'success',
+      variant: 'weak',
+      class: ['bg-success-4 text-success-hi hover:bg-success-5 active:bg-success-6'],
+    },
+    {
+      intent: 'info',
+      variant: 'weak',
+      class: ['bg-info-4 text-info-hi hover:bg-info-5 active:bg-info-6'],
+    },
+    {
+      intent: 'warning',
+      variant: 'weak',
+      class: ['bg-warning-4 text-warning-11 hover:bg-warning-5 active:bg-warning-6'],
+    },
+    // ghost
+    {
+      intent: 'neutral',
+      variant: 'ghost',
+      class: ['text-base-hi hover:bg-base-4 active:bg-base-5'],
+    },
+    {
+      intent: 'primary',
+      variant: 'ghost',
+      class: ['text-primary-hi hover:bg-primary-4 active:bg-primary-5'],
+    },
+    {
+      intent: 'danger',
+      variant: 'ghost',
+      class: ['text-danger-hi hover:bg-danger-4 active:bg-danger-5'],
+    },
+    {
+      intent: 'success',
+      variant: 'ghost',
+      class: ['text-success-hi hover:bg-success-4 active:bg-success-5'],
+    },
+    {
+      intent: 'info',
+      variant: 'ghost',
+      class: ['text-info-hi hover:bg-info-4 active:bg-info-5'],
+    },
+    {
+      intent: 'warning',
+      variant: 'ghost',
+      class: ['text-warning-11 hover:bg-warning-4 active:bg-warning-5'],
+    },
+    // outline
+    {
+      intent: 'neutral',
+      variant: 'outline',
+      class: ['bg-base-1 border-base-7 text-base-hi hover:border-base-8 hover:bg-base-1'],
+    },
+    {
+      intent: 'primary',
+      variant: 'outline',
+      class: [
+        'bg-primary-1 border-primary-7 text-primary-hi hover:border-primary-8 hover:bg-primary-1',
+      ],
+    },
+    {
+      intent: 'danger',
+      variant: 'outline',
+      class: ['bg-danger-1 border-danger-7 text-danger-hi hover:border-danger-8 hover:bg-danger-1'],
+    },
+    {
+      intent: 'success',
+      variant: 'outline',
+      class: [
+        'bg-success-1 border-success-7 text-success-hi hover:border-success-8 hover:bg-success-1',
+      ],
+    },
+    {
+      intent: 'info',
+      variant: 'outline',
+      class: ['bg-info-1 border-info-7 text-info-hi hover:border-info-8 hover:bg-info-1'],
+    },
+    {
+      intent: 'warning',
+      variant: 'outline',
+      class: [
+        'bg-warning-1 border-warning-7 text-warning-11 hover:border-warning-8 hover:bg-warning-1',
+      ],
+    },
+  ],
+  defaultVariants: {
+    intent: 'primary',
+    size: 'medium',
+    variant: 'solid',
+  },
+});
