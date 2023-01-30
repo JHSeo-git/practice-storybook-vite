f318f76e7ef922da292f0342e05a44894848041b
JHSeo <qnfqnfqnf@gmail.com>
2023-01-30 22:07:25 +0900

    chore: add lib bundling configuration

--- vite.lib.config.ts
+++ vite.lib.config.ts
@@ -1,7 +1,35 @@
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
+import external from 'rollup-plugin-peer-deps-external';
+import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+const OUT_DIR = 'dist-lib';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    minify: false,
+    target: 'esnext',
+    outDir: OUT_DIR,
+    lib: {
+      entry: './src/index.ts',
+      name: 'PracticeStorybookViteLibrary',
+      formats: ['es', 'cjs'],
+      fileName: '[name]',
+    },
+    rollupOptions: {
+      output: {
+        preserveModules: true,
+      },
+    },
+  },
+  plugins: [
+    dts({ outputDir: OUT_DIR, entryRoot: '.' }),
+    react(),
+    tsconfigPaths(),
+    external(),
+    visualizer(),
+  ],
 });
