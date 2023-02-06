98919af930e4b81e2c05d9052990919746f4481d
JHSeo <qnfqnfqnf@gmail.com>
2023-02-06 22:54:17 +0900

    initialize nextra docs

---
+++ docs/README.md
@@ -0,0 +1,5 @@
+# Practice Documentation
+
+This is the documentation for the Practice project.
+
+- [nextra](https://nextra.site) is used for the documentation site.
---
+++ docs/pages/_app.tsx
@@ -0,0 +1,8 @@
+import '../style.css';
+
+import type { AppProps } from 'next/app';
+import type { ReactElement } from 'react';
+
+export default function Nextra({ Component, pageProps }: AppProps): ReactElement {
+  return <Component {...pageProps} />;
+}
